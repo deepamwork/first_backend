f7e6f937e0a3c17681c9f3d16c8f4e5ce7e824bb
pratham <jhahiren36@gmail.com>
2024-01-10 16:42:46 +0530

    done

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('chai pilo');
+  res.send('coffee pilo');
 });
 
 module.exports = router;
